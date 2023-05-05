f98ddadf16944b4c399f6ef9b46b39579c29a0d0
afratetlay <afratetlay@gmail.com>
2023-05-05 12:19:48 +0200

    adding the o and x into the game

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,26 @@
-const Cell = ({ id, cell, setCells }) => {
-  const handleClick = (event) => {
+const Cell = ({ id, cell, setCells, go, setGo }) => {
+  const handleClick = (e) => {
     const taken =
-      event.target.firstChild.classList.contains("circle") ||
-      event.target.firstChild.classList.contains("cross");
+      e.target.firstChild.classList.contains("circle") ||
+      e.target.firstChild.classList.contains("cross");
+
+    if (!taken) {
+      if (go === "circle") {
+        e.target.firstChild.classList.add("circle");
+        setGo("cross");
+      }
+      if (go === "cross") {
+        e.target.firstChild.classList.add("cross");
+        setGo("circle");
+      }
+    }
   };
 
-  return <div className="square" id={id} onClick={handleClick}></div>;
+  return (
+    <div className="square" id={id} onClick={handleClick}>
+      <div className={cell}></div>
+    </div>
+  );
 };
 
 export default Cell;
